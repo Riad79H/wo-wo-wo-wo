094bf26ccf735e98cffaf9d39ac88f9afbfeeb7a
Riad79H <onlypc67@gmail.com>
2025-05-21 13:56:07 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿document.querySelector(".background").addEventListener("click", () => {
+  document.getElementById("message").style.display = "block";
+});
+
+// Random flowers around
+const flowerContainer = document.querySelector(".flowers");
+for (let i = 0; i < 30; i++) {
+  const flower = document.createElement("div");
+  flower.classList.add("flower");
+  flower.style.top = Math.random() * 100 + "%";
+  flower.style.left = Math.random() * 100 + "%";
+  flowerContainer.appendChild(flower);
+}
